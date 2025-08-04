9013a3f2da04625530d44f14dc247db59fa182c6
ThunderTrader <thunder.trader.business@gmail.com>
2025-08-04 12:32:14 -0400

    upload

--- arbitrage/pair_plot.py
+++ arbitrage/pair_plot.py
@@ -34,22 +34,27 @@ def load_tick_data_by_name(instrument, start_data):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    serial_0 = "sfit.future.bc2509"
+    serial_0 = "ibkr.tws.l1.COMEX.651096940"
     serial_1 = "ibkr.tws.l1.COMEX.447585333"
-    serial_2 = "sfit.future.cu2509"
-    ticks = ptu.load_from_data_center([serial_0, serial_1, serial_2], START_DATE, "20250801", DATA_CENTER)
-    serials = ptu.align_ticks_by_window(ticks, 1000)
-    last_price_0 = [t.last_price * 7.1 * 25000 * 5 / 11 if t is not None else float("NaN") for t in serials[serial_0]]
-    last_price_1 = [t.last_price * 5.0 if t is not None else float("NaN")for t in serials[serial_1]]
-    last_price_2 = [t.last_price * 7.1 * 25000 * 5 / 11 if t is not None else float("NaN") for t in serials[serial_2]]
+    ticks = ptu.load_from_data_center([serial_0, serial_1], START_DATE, "20250801", DATA_CENTER)
+    serials = ptu.align_ticks_by_slot(ticks)
+
+    #last_price_0 = [t.last_price * 7.1 * 25000 * 5 / 11 if t is not None else float("NaN") for t in serials[0]]
+    #last_price_1 = [t.last_price * 5.0 if t is not None else float("NaN")for t in serials[1]]
+    last_price_0 = [t.last_price if t is not None else float("NaN") for t in serials[0]]
+    last_price_1 = [t.last_price if t is not None else float("NaN")for t in serials[1]]
     last_price_diff = [last_price_0[i] - last_price_1[i] for i in range(0, len(last_price_0))]
+
     fig = plt.figure(tight_layout=True, figsize=(32, 18))
-    ax = fig.add_subplot()
-            
-    ax.plot(last_price_0, color='tab:green', label=serial_0)
-    ax2 = ax.twinx()
-    ax2.plot(last_price_1, color='tab:red',label=serial_1)
-    ax3 = ax.twinx()
-    ax3.plot(last_price_2, color='tab:blue',label=serial_2)
-    ax.legend(prop={'size': 20}, framealpha=0.0, fancybox=False)
-    plt.savefig("%s/%s-%s.png" % (REPORT_PATH, serial_0, serial_1), dpi=50, transparent=True)
+
+    gs = gridspec.GridSpec(2, 1)
+    price_fig_left = fig.add_subplot(gs[0, 0])
+    price_fig_left.plot(last_price_0, color='tab:green', label=serial_0)
+    price_fig_right  = price_fig_left.twinx()
+    price_fig_right.plot(last_price_1, color='tab:red',label=serial_1)
+    diff_fig = fig.add_subplot(gs[1, 0])
+    diff_fig.plot(last_price_diff, color='tab:red', label="diff")
+
+    fig.legend(prop={'size': 20}, framealpha=0.0, fancybox=False)
+
+    plt.savefig("%s/%s-%s.png" % (REPORT_PATH, serial_0, serial_1), dpi=100, transparent=False)
